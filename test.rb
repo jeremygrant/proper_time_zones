2878d1fb674bde466a956d8420f306004b00afe4
Jeremy Grant <jgrant@deaths-head.local>
2009-10-08 16:12:03 +1100

    Added clearer and more precise tests for testing the accuracy of Time zoned attributes. Hopefully I won't ever have to do this again

--- test.rb
+++ test.rb
@@ -5,57 +5,95 @@ require 'bacon'
 require 'facon'
 require 'sqlite3'
 
+# setting the default timezone to UTC
 Time.zone_default = 'UTC'
-Time.zone = 'UTC'
+puts Time.zone
+
+# setting up active record like rails
 ActiveRecord::Base.time_zone_aware_attributes = true
 ActiveRecord::Base.default_timezone = :utc
 ActiveRecord::Base.logger = Logger.new STDOUT
 
-ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database  => "test.sqlite3"
-# ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => "proper_time_zones_test", :username => "carl",
-#                                         :password => '')
+# connection to the database
+#ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database  => "test.sqlite3"
+ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => "proper_time_zones_test", :username => "jgrant", :password => '')
 
-class CreateEvents < ActiveRecord::Migration
+class CreateArticles < ActiveRecord::Migration
   def self.up
-    create_table :events do |t|
-      t.datetime :scheduled_at
-      t.date :scheduled_on
+    create_table :articles do |t|
+      t.timestamp :published_at
       t.timestamps
     end
   end
 
   def self.down
-    drop_table :events
+    drop_table :articles
   end
 end
 
-class Event < ActiveRecord::Base; end
+class Article < ActiveRecord::Base
+  named_scope :by_published, lambda{ |published_at|
+    start_of_published_on = Time.zone.local_to_utc(published_at.to_date.to_time)
+    # both of the following work to the second which appears to be the best precision that ruby Time has
+    {:conditions => ["published_at between ? and ?", start_of_published_on, start_of_published_on + 1.day - 1.second]}
+    #{:conditions => ["published_at >= ? and published_at < ?", start_of_published_on, (start_of_published_on + 1.day)]}
+  }
 
-CreateEvents.down rescue puts("Down failed because db doesn't exist")
-CreateEvents.up
+  def published_on
+    published_at.to_date
+  end
+end
 
-describe "time zones messing up" do
-  before do
-    Time.zone = "Sydney"
-    @event = Event.create! :scheduled_at => DateTime.now, :scheduled_on => Date.today
-  end
-  after do
-    Event.destroy_all
-  end
-  describe "find by date field" do
-    it "should find an event on today" do
-      today = Time.parse Date.today.strftime('%Y-%m-%d')
-      Event.find_by_scheduled_on(today.to_date).should.equal @event
-    end
-  end
-  describe "find by the time field" do
-    it "should find an event with time on a date with a parsed time" do
-      today = Time.parse Date.today.strftime('%Y-%m-%d')
-      Event.find(:first, :conditions => ["date(scheduled_at) = date(?)", today.utc]).should.equal @event
-    end
-    it "should find an event with time on a date with today" do
-      today = Time.today
-      Event.find(:first, :conditions => ["date(scheduled_at) = date(?)", today.utc]).should.equal @event
+CreateArticles.down rescue nil
+CreateArticles.up
+
+describe Article do
+  %w(Samoa UTC Sydney Nuku'alofa).each do |timezone|
+    describe "testing single time zone #{timezone}" do
+      before do
+        Time.zone = timezone
+        @end_of_the_previous_day = Time.zone.parse "2009-10-07 23:59:59.999999"
+        @start_of_the_day = Time.zone.parse "2009-10-08 00:00:00.000000"
+        @end_of_the_day = Time.zone.parse "2009-10-08 23:59:59.999999"
+        @start_of_the_next_day = Time.zone.parse "2009-10-09 00:00:00.000000"
+        Article.create! :published_at => @end_of_the_previous_day
+        Article.create! :published_at => @start_of_the_day
+        Article.create! :published_at => @end_of_the_day
+        Article.create! :published_at => @start_of_the_next_day
+      end
+
+      after do
+        Article.destroy_all
+      end
+
+      it "should find the 2 articles on the local date 2009-10-08 using the start of the day" do
+        time = @start_of_the_day
+        Article.by_published(time).each do |article|
+          article.published_on.to_s(:long).should.equal "October  8, 2009"
+        end
+      end
+
+      it "should find 2 on the local date 2009-10-08 articles using the end of the day" do
+        time = @end_of_the_day
+        Article.by_published(time).each do |article|
+          article.published_on.to_s(:long).should.equal "October  8, 2009"
+        end
+      end
+
+      it "should find 2 on the local date 2009-10-08 articles using some arbitrary time of the day" do
+        time = Time.zone.parse "2009-10-08 12:34:56.789012"
+        Article.by_published(time).each do |article|
+          article.published_on.to_s(:long).should.equal "October  8, 2009"
+        end
+      end
+
+      it "should find 2 on the local date 2009-10-08 articles using some arbitrary time of the day" do
+        time = Date.parse "2009-10-08"
+        Article.by_published(time).each do |article|
+          article.published_on.to_s(:long).should.equal "October  8, 2009"
+        end
+      end
+
     end
   end
 end
